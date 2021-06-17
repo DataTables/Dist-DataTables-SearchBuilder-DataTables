9c7bb129ab0c5e5f6264a5c294a89b4a4b12d2d6
Allan Jardine <allan.jardine@sprymedia.co.uk>
2021-06-17 16:56:47 +0100

    Sync tag release - 1.1.0

--- js/searchBuilder.dataTables.js
+++ js/searchBuilder.dataTables.js
@@ -1,43 +1,36 @@
-/*! SearchBuilder 1.0.1
- * ©2020 SpryMedia Ltd - datatables.net/license/mit
+/*! DataTables integration for DataTables' SearchBuilder
+ * ©2016 SpryMedia Ltd - datatables.net/license
  */
-(function () {
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery', 'datatables.net-dt', 'datatables.net-searchbuilder'], function ($) {
+            return factory($, window, document);
+        });
+    }
+    else if (typeof exports === 'object') {
+        // CommonJS
+        module.exports = function (root, $) {
+            if (!root) {
+                root = window;
+            }
+            if (!$ || !$.fn.dataTable) {
+                // eslint-disable-next-line @typescript-eslint/no-var-requires
+                $ = require('datatables.net-dt')(root, $).$;
+            }
+            if (!$.fn.dataTable.searchBuilder) {
+                // eslint-disable-next-line @typescript-eslint/no-var-requires
+                require('datatables.net-searchbuilder')(root, $);
+            }
+            return factory($, root, root.document);
+        };
+    }
+    else {
+        // Browser
+        factory(jQuery, window, document);
+    }
+}(function ($, window, document) {
     'use strict';
-
-    /*! DataTables integration for DataTables' SearchBuilder
-     * ©2016 SpryMedia Ltd - datatables.net/license
-     */
-    (function (factory) {
-        if (typeof define === 'function' && define.amd) {
-            // AMD
-            define(['jquery', 'datatables.net-dt', 'datatables.net-searchbuilder'], function ($) {
-                return factory($, window, document);
-            });
-        }
-        else if (typeof exports === 'object') {
-            // CommonJS
-            module.exports = function (root, $) {
-                if (!root) {
-                    root = window;
-                }
-                if (!$ || !$.fn.dataTable) {
-                    // eslint-disable-next-line @typescript-eslint/no-var-requires
-                    $ = require('datatables.net-dt')(root, $).$;
-                }
-                if (!$.fn.dataTable.searchBuilder) {
-                    // eslint-disable-next-line @typescript-eslint/no-var-requires
-                    require('datatables.net-searchbuilder')(root, $);
-                }
-                return factory($, root, root.document);
-            };
-        }
-        else {
-            // Browser
-            factory(jQuery, window, document);
-        }
-    }(function ($, window, document) {
-        var dataTable = $.fn.dataTable;
-        return dataTable.searchPanes;
-    }));
-
-}());
+    var dataTable = $.fn.dataTable;
+    return dataTable.searchPanes;
+}));
