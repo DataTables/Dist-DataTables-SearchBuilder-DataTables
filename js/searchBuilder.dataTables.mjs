7538156d9bff8f1b2deae5c5497c7f7685261440
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:07 +0000

    Add support for mjs files (ES Modules)

---
+++ js/searchBuilder.dataTables.mjs
@@ -0,0 +1,13 @@
+
+/*! DataTables integration for DataTables' SearchBuilder
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-dt';
+import DataTable from 'datatables.net-searchbuilder';
+
+
+
+
+export default DataTable;
